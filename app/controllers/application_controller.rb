49af6787766a3944482d2c1e20f0f04d7d4230f0
paige <paigetetz@gmail.com>
2023-01-09 15:36:50 -0500

    Possible Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+
+  private
+  
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { error: 'Unauthorized' }, stautus: :unauthorized
+  end
 end
