e6697dd83fded3942a41e0247bb7adff14d603f4
paige <paigetetz@gmail.com>
2023-01-10 12:56:26 -0500

    Seed file done, backend CRUD started

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,22 @@
 class BooksController < ApplicationController
+    skip_before_action :authorize, only: [:index, :show, :create, :update, :destroy]
+    def index
+        render json: Book.all
+    end
+
+    def create
+        book = Book.create(book_params)
+            if book.valid?
+                render json: book, status: :created
+            else
+                render json: {error: book.errors.full_messages}, status: :unprocessable_entity
+            end
+    end
+
+
+    private
+
+    def book_params
+        params.permit(:title, :author, :summary, :page_count, :genre, :published, :rating)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+    skip_before_action :authorize, only: [:index, :show, :create, :update, :destroy]
+    def index
+        render json: Review.all
+    end
+
+    def create
+        review = Review.create(review_params)
+            if review.valid?
+                render json: review, status: :created
+            else
+                render json: {error: review.errors.full_messages}, status: :unprocessable_entity
+            end
+    end
+
+    private
+
+    def review_params
+        params.permit(:user_id, :book_id, :headline, :commentary, :rating, :likes)
+    end
 end
