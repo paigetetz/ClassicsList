7b9b928666678c0d7028d6ffaba01e258a40cab2
paige <paigetetz@gmail.com>
2023-01-12 01:36:35 -0500

    Reviews rendered, more routing

--- client/src/Component/BookShow.js
+++ client/src/Component/BookShow.js
@@ -1,11 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
-
-
+import ReviewCard from './ReviewCard'
 function BookShow(props) {
     const [book, setBook] = useState([]);
     const {id} = useParams();
-
     useEffect(() => {
         fetch(`/books/${id}`)
         .then((response) => response.json())
@@ -13,6 +11,18 @@ function BookShow(props) {
             setBook({...data});
         })
     },[id]);
+    // console.log(book.reviews);
+        const reviews = book.reviews;
+        console.log(reviews);
+        function renderReviews(reviews) {
+            if(reviews) {
+                return reviews.map(review => <ReviewCard key={review.id} review={review} />)
+            }
+            else 
+                return <div></div>
+        }
+            
+        
 
     return (
         <div>
@@ -24,10 +34,11 @@ function BookShow(props) {
             <h5>{book.published}</h5>
             <h5>{book.page_count}</h5>
             <p>{book.summary}</p>
-
+            <div className='review-card-container'>
+                {renderReviews(reviews)}
+            </div>
             
         </div>
     );
 }
-
 export default BookShow;
--- client/src/Component/ReviewCard.js
+++ client/src/Component/ReviewCard.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
-function ReviewCard(props) {
+import { Link } from 'react-router-dom';
+function ReviewCard({review}) {
+    const fullReview = review.commentary
+    const summary = fullReview.substring(0, 50) + "...";
+    // const averageRating = review.rating.all/ review.length
+    // console.log(averageRating)
+    
     return (
-        <div>
-            
+        <div className='review-card'>
+            <h2>{review.headline}</h2>
+            <br></br>
+            <h4>{review.rating}/5</h4>
+            <br></br>
+            <h6>By: {review.user_id}</h6>
+            <br></br>
+            <p>{summary}</p>
+            <Link to={`/review/${review.id}`}>
+            <p>Continue?</p>
+            </Link>
         </div>
     );
 }
-
-export default ReviewCard;
+export default ReviewCard
