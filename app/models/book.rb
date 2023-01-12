90fb24bd4b40761459756359eea2d392383e3d37
paige <paigetetz@gmail.com>
2023-01-12 16:33:10 -0500

    Profile Page outline

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,16 @@ class Book < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
 
+    # def average_rating
+    #     self.reviews.average(:rating)
+    # end
+    # def average_rating(book)
+    #     reviews = Review.where(book_id: book_id)
+    #     total = 0
+    #     reviews.each do |review|
+    #         total += review.rating
+    #     end
+    #     average = total.to_f / reviews.length
+    #     average
+    # end
 end
--- client/src/Component/Profile.js
+++ client/src/Component/Profile.js
@@ -1,9 +1,15 @@
-import React from 'react';
 
-function Profile(props) {
+
+function Profile({user}) {
+    // const myReviews = user.reviews
+    // console.log(myReviews)
     return (
-        <div>
-            
+        <div className='profile'>
+            <h1>Hello, {user.username}</h1>
+            <img className='profile-image' src={user.profile_pic} alt={user.username}/>
+            <h3>Favorite Book: {user.fav_book}</h3>
+            <h3>Favorite Genre: {user.fav_genre}</h3>
+
         </div>
     );
 }
